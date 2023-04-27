29dc10716c56e1e28a7b44d84732e145082f62bf
wangzhi <wangzhi@cainiaomeishi.com>
2023-04-27 13:43:03 +0800

    优化android蓝牙扫描结果，增加蓝牙类型

---
+++ android/src/main/java/it/innove/BleAction.java
@@ -0,0 +1,8 @@
+package it.innove;
+
+public class BleAction {
+    public final static int BleManagerStopScan = 0;
+    public final static int BluetoothStartScan = 1;
+    public final static int BleManagerDiscover = 2;
+
+}
